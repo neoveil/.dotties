e875df8a3a513f921b7403b53fae1ec38bd748c3
Lucas A. F. Vitor <luca.oetdbem@gmail.com>
2024-09-04 21:57:17 -0300

    move `gpg` to `.creds`/include `grip`/aliases for octal permissions ls

---
+++ grip/.grip/settings.py
@@ -0,0 +1,13 @@
+def get_token_from_keyring():
+  import subprocess
+  cmd = "lssecret -s | grep -i -A1 'grip' | tail -n 1 | sed 's/^secret:[[:space:]]//I'"
+  process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  output, error = process.communicate()
+
+  if process.returncode != 0:
+    raise Exception(f"error retrieving access token from keyring: {error.decode().strip()}")
+
+  return output.decode().strip()
+
+PASSWORD = get_token_from_keyring()
+USERNAME = 'lucasarthur'
