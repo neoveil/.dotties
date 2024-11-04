aaa4debf959cd8aaaa276a47c2193c5519b90fd3
Lucas A. F. Vitor <luca.oetdbem@gmail.com>
2024-11-04 08:37:11 -0300

    include `lsp-java-lombok`

---
+++ emacs/.emacs.d/local/lsp-java-lombok.el
@@ -0,0 +1,54 @@
+;; -*- lexical-binding: t; -*-
+
+(defcustom lsp-java-lombok-enabled t
+  "If non-nil starts provides lombok support to JDTLS."
+  :type 'boolean
+  :group 'lsp-java-lombok)
+
+(defcustom lsp-java-lombok-version nil
+  "If non-nil, use the specified lombok version, otherwise use the latest."
+  :type '(choice (string)
+                 (const nil))
+  :group 'lsp-java-lombok)
+
+(defcustom lsp-java-lombok-install-dir (file-name-concat user-emacs-directory ".cache")
+  "The path on disk where lombok jars are installed."
+  :type 'directory
+  :group 'lsp-java-lombok)
+
+(defconst lsp-java-lombok--jar-base-url "https://projectlombok.org/downloads/"
+  "The base path to download lombok jars from.")
+
+(defun lsp-java-lombok--jar-file ()
+  "Get the filename for the lombok jar."
+  (concat "lombok"
+          (when lsp-java-lombok-version "-")
+          lsp-java-lombok-version
+          ".jar"))
+
+(defun lsp-java-lombok--jar-path ()
+  "Generate the path on disk for the lombok jar."
+  (file-name-concat lsp-java-lombok-install-dir (lsp-java-lombok--jar-file)))
+
+(defun lsp-java-lombok--install-jar ()
+  "Download and install the lombok jar."
+  (let* ((lombok-url (url-generic-parse-url lsp-java-lombok--jar-base-url))
+         (base-path (file-name-as-directory (url-filename lombok-url)))
+         (file-path (file-name-concat base-path (lsp-java-lombok--jar-file))))
+    (setf (url-filename lombok-url) file-path)
+    (url-copy-file lombok-url (lsp-java-lombok--jar-path) t)))
+
+(defun lsp-java-lombok--append-vmargs ()
+  "Apply lombok args to lsp-java-vmargs."
+  (defvar lsp-java-vmargs)
+  (add-to-list 'lsp-java-vmargs (concat "-javaagent:" (lsp-java-lombok--jar-path)) t))
+
+(defun lsp-java-lombok-setup ()
+  "Setup lsp-java-lombok."
+  (when lsp-java-lombok-enabled
+    (when (not (file-exists-p (lsp-java-lombok--jar-path)))
+      (message "Could not find lombok for lsp-java. Installing...")
+      (lsp-java-lombok--install-jar))
+    (lsp-java-lombok--append-vmargs)))
+
+(provide 'lsp-java-lombok)
